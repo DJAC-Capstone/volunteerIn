4799a22b7ee028f7a68b46bdb6207c46f9b22d41
Dzmitry Akulau <dmitry@Dzmitrys-MBP.home>
2020-12-01 01:25:59 -0500

    updatedRedux

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(authMiddleware);
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/api', require('./api'));
+//app.use('/api', require('./api'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.get('*', (req, res) => {
