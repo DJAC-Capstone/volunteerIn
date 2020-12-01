449ae8c74788c8428e0a42c389d310c4b0f54865
Cynthia E <cynthia8499@hotmail.com>
2020-11-30 20:15:07 -0600

    made changes to navbar and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
-  render() {
-    return <div>This will be the login page :)</div>;
-  }
+	render() {
+		return <div>This will be the login page :)</div>;
+	}
+}
+import React, { Component } from 'react';
+import userStyles from '../Utils/userStyles';
+
+export default class Login extends Component {
+	render() {
+		return (
+			<div>
+				<form style={userStyles}>
+					<div style={{ margin: '20px' }}>
+						<h3>Login to VolunteerIn</h3>
+						<div className="form-group">
+							<input
+								type="email"
+								className="form-control"
+								id="email"
+								autoComplete="email"
+								placeholder="Email address"
+							/>
+						</div>
+						<div className="form-group">
+							<input
+								type="password"
+								className="form-control"
+								id="password"
+								autoComplete="current-password"
+								placeholder="Password"
+							/>
+						</div>
+						<button type="submit" className="btn btn-primary">
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
