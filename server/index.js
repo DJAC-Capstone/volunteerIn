3ba1101f5ed08d5cc0e231a18e93be41ab2dd267
Jamil Kabir <jamilkabir85@gmail.com>
2020-12-03 02:49:43 -0500

    added styling to home-page without using the db

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ app.use(express.json());
 //app.use('/api', require('./api'));
 
 app.use(express.static(path.join(__dirname, '/public')));
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, './public/index.html'));
-// });
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.use('/api', require('./routes'));
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,52 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <div>This will be the home page :)</div>;
+    // return <div>This will be the home page :)</div>;
+    return (
+      <div className="main-container">
+        <div className="user-post-container">
+          <input placeholder="Share A Thought"></input>
+          <div className="other-type-of-posts-container">
+            <button>Create Event</button>
+            <button>Upload Photo</button>
+            <button>Upload Video</button>
+          </div>
+        </div>
+
+        <div className="other-users-post-container">
+          <div className="profile-pic-and-name-container">
+            <img src="https://randomuser.me/api/portraits/men/34.jpg" />
+            <div className="other-users-post-text">
+              <p className="full-name">Alex Baker (EC: 10, EA: 45)</p>
+              <p className="user-thoughts">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="other-users-post-container">
+          <div className="profile-pic-and-name-container">
+            <img src="https://randomuser.me/api/portraits/women/8.jpg" />
+            <div className="other-users-post-text">
+              <p className="full-name">Mary Klein (EC: 1, EA: 2)</p>
+              <p className="user-thoughts">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="other-users-post-container">
+          <div className="profile-pic-and-name-container">
+            <img src="https://randomuser.me/api/portraits/women/89.jpg" />
+            <div className="other-users-post-text">
+              <p className="full-name">Eva Rhodes (EC: 6, EA: 5)</p>
+              <p className="user-thoughts">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+      
+    )
   }
 }
