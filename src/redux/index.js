09a57f74c90da8c81929e22b4f39a37a0073fbad
antonpreku <antonpreku@hotmail.com>
2020-12-02 23:36:20 -0500

    chat box

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-import userReducer from './users';
+import usersReducer from './users';
 import eventReducer from './Events';
 
 const appReducer = combineReducers({
 
-  evets: eventReducer,
-  users: userReducer,
+  evet: eventReducer,
+  user: usersReducer,
 
 });
 
