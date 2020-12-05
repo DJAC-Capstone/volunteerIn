75963463cba9e65835b574df62c211547cd9d146
Jamil Kabir <jamilkabir85@gmail.com>
2020-12-04 23:20:22 -0500

    home-page functional - just need to add more data now

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,46 +3,59 @@ import { connect } from 'react-redux';
 
 
 import  { getEvents } from '../redux/events'
+import { getAllUsers } from '../redux/users'
 export class Home extends Component {
   constructor() {
     super()
     this.state = {
-      events: []
+      events: [],
+      allUsers: []
     }
   }
 
   async componentDidMount() {
-    
+    await this.props.fetchAllUsers()
     await this.props.fetchEvents()
     this.setState({
-      events: this.props.events
+      events: this.props.events,
+      allUsers: this.props.allUsers
     });
   }
   
   render() {
-   
-    //Still need to connect users on redux    
+
+    //Still need to connect users on redux
     return (
       <div className="main-container">
-        <div className="user-post-container">
-          <input placeholder="Share A Thought"></input>
-          <div className="other-type-of-posts-container">
-            <button>Create Event</button>
-            <button>Upload Photo</button>
-            <button>Upload Video</button>
+        <div className="left-container">
+          <img src={`https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * (40 - 1) + 1)}.jpg`}/>
+          <p>Person's Name</p>
+          <div className="user-post-container">
+            <input placeholder="Share A Thought"></input>
+            <div className="other-type-of-posts-container">
+              <button>Create Event</button>
+              <button>Upload Photo</button>
+              <button>Upload Video</button>
+            </div>
           </div>
         </div>
+        
 
-        <div>
+        <div className="middle-container">
           {this.state.events.map (ev=>{
+            let randID = Math.floor(Math.random() * (3 - 1) + 1)
             return (
               <div key={ev.id} className="other-users-post-container">
                 <div className="profile-pic-and-name-container">
                   <img src={`https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * (40 - 1) + 1)}.jpg`}/>
-                  <div className="other-users-post-text">
-                    <p className="full-name">Person's Name (EC: 1, EA: 2)</p>
-                    <p className="event-description">{ev.description}</p>
-                  </div>
+                  {/* {this.state.allUsers.users.map(usr=>{
+                    return ( */}
+                    <div className="other-users-post-text">
+                      <p className="full-name">{`${this.state.allUsers.users[randID].first_name} ${this.state.allUsers.users[randID].last_name}`}</p>
+                      <p className="event-description">{ev.description}</p>
+                    </div>
+                    {/* )
+                  })} */}
                 </div>
               </div>
            )
@@ -57,12 +70,14 @@ export class Home extends Component {
 const mapStateToProps = (state) => {
   return {
     events: state.events,
+    allUsers: state.users
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchEvents: () => dispatch(getEvents()),
+    fetchAllUsers: () => dispatch(getAllUsers())
   }
 }
 
