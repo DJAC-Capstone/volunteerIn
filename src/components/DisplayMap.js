92c5b0d03b24002811d22746682e2f507f309c6e
Cynthia E <cynthia8499@hotmail.com>
2020-12-10 22:49:09 -0600

    more changes

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
+import { connect } from 'react-redux';
 import { GoogleMap, LoadScript, GoogleMapPin, Marker } from '@react-google-maps/api';
 import { MAP_API } from './secrets'
  
@@ -9,7 +10,7 @@ const containerStyle = {
 };
  
  
-function DisplayMaps() {
+function DisplayMaps({event}) {
   console.log(event)
   const [map, setMap] = useState(null);
   const [loc, setLoc] = useState({lat:0, lng:0});
@@ -23,15 +24,11 @@ function DisplayMaps() {
     zip_code
   }
 
-
-
   const stringAdd = Object.keys(newAdd).reduce(function(acc, v) {
       return acc.concat(' ', `${newAdd[v]},`);
   }, '');
  
   
-
-
   const onLoad = useCallback(function callback(map) {
   }, [])
  
@@ -41,19 +38,24 @@ function DisplayMaps() {
  
   const getLatAndLongFromAddress = async (add) => {
     // im gonna translate the address to a lat and long
-    add = encodeURIComponent(add);
+    add = !!event && encodeURIComponent(add);
+    console.log(add)
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${add}&key=${MAP_API}`;
     const { data } = await axios.get(url);
     const  { lat, lng } = data.results[0].geometry.location;
+    console.log(url)
+    console.log(add)
+    console.log(data)
     console.log(lat, lng)
     setLoc({lat: lat, lng:lng})
   };
 
 
   useEffect(() => {
+    
     const address = stringAdd;
     getLatAndLongFromAddress(address);
-
+console.log(address)
   }, []); 
 
   return (
@@ -77,4 +79,11 @@ function DisplayMaps() {
   )
 }
  
-export default React.memo(DisplayMaps)
+export default connect(
+    (state) => ({
+        event:  state.events.event,
+		user: state.users.user
+
+      }),
+)(DisplayMaps)
+
