58bf9c5acc710066fd7a273cb14355486fdf3381
Jamil Kabir <jamilkabir85@gmail.com>
2020-12-12 01:22:22 -0500

    added more features to home page - waiting on post component to add more

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,8 @@ export class Home extends Component {
     this.state = {
       events: [],
       allUsers: [],
-      userName: '',
-      dummyUsers:[],
-      dummyUser:{}
+      userEvents: []
     }
-    console.log('111',this.props)
 
     this.createEventButton = this.createEventButton.bind(this)
     // this.handleUserNameClick = this.handleUserNameClick.bind(this)
@@ -29,31 +26,42 @@ export class Home extends Component {
     this.setState({
       events: this.props.events,
       allUsers: this.props.allUsers.users,
-      user: this.props.allUsers.user
+      user: this.props.allUsers.user,
+      userEvents: this.props.events.users
     })
   }
+
+  // componentDidUpdate(){
+  //   if(this.props.events[0].users === undefined){
+  //     this.fetchEvents
+  //   }
+  //   console.log("........", this.state)
+  // }
 
   createEventButton () {
     window.location.hash = "#/createEvent"
   }
 
   render() {
-    console.log("2312321312",this.props)
+    console.log("props in render", this.props)
     const {user} = this.props.allUsers
     const {users} = this.props.allUsers
-    const {events} = this.props
-    let i = 0;
+    let i = 0
+    const userEvents = this.props.allUsers.users
+
     return (
       <div className="main-container">
         <div className="left-container">
-          <img src={`https://randomuser.me/api/portraits/women/${++i}.jpg`}/>
-          <Link to={{
-            pathname: `/${user.first_name}${user.last_name}`,
-            // state: {
-            //   events: events,
-            //   user: user
-            // }
-          }}>{`${user.first_name} ${user.last_name}`}</Link>
+          <div className="profileImg-name">
+            <img className="image-profile" src={`https://randomuser.me/api/portraits/women/${++i}.jpg`}/>
+            <Link to={{
+              pathname: `/${user.first_name}${user.last_name}`,
+              // state: {
+              //   events: events,
+              //   user: user
+              // }
+            }}>{`${user.first_name} ${user.last_name}`}</Link>
+          </div>
           <div className="user-post-container">
             <input placeholder="Post"></input>
             <div className="other-type-of-posts-container">
@@ -62,10 +70,42 @@ export class Home extends Component {
               <button>Post</button>
             </div>
           </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/volunteers-cleaning-park-picture-id986900214?k=6&m=986900214&s=612x612&w=0&h=XaXvsvgJt1keusMMkOyXF8xpOxVLI3znd0SWRFSYxvY=" />
+          </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/donate-to-the-poor-homeless-still-seen-in-society-concept-of-charity-picture-id1082704324?k=6&m=1082704324&s=612x612&w=0&h=o4mrYDjCvudoAexUUthn8GtB2Q_hHasHmek7YHPy0cw=" />
+          </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/people-planting-tree-in-park-picture-id1022255954?k=6&m=1022255954&s=612x612&w=0&h=kGMlSfixvVec2FSsrmoE785K5_m4pPcj0tYl3TdKYvk=" />
+          </div>  
         </div>
      
         <div className="middle-container">
-          
+          {user.friends.map (friendID=>{
+            return (
+              <div key={++i} className="other-users-post-container">
+                <div className="profile-pic-and-name-container">
+                  <img className="image-friend" src={`https://randomuser.me/api/portraits/women/${i}.jpg`}/>
+                  <div>
+                    <p>{`${users[friendID].first_name} ${users[friendID].last_name}`}</p>
+                  {/* <div>
+                    <p className='event-title-homepage'>{`${users[friendID].events[0].title}`}</p>
+                    <p className='event-description-homepage'>{`${users[friendID].events[0].description}`}</p>
+                  </div> */}
+                </div>
+                    )
+                </div>
+                <div>
+                  <p className='event-title-homepage'>{`${users[friendID].events[0].title}`}</p>
+                  <p className='event-description-homepage'>{`${users[friendID].events[0].description}`}</p>
+                </div>
+              </div>
+           )
+           })}
         </div>
       </div>
     )
