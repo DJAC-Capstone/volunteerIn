3a65e1ba27355a4f905cfc127c2d5bd421bae7fe
Jamil Kabir <jamilkabir85@gmail.com>
2020-12-12 12:14:24 -0500

    fixed a few bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,7 +47,6 @@ export class Home extends Component {
     const {user} = this.props.allUsers
     const {users} = this.props.allUsers
     let i = 0
-    const userEvents = this.props.allUsers.users
 
     return (
       <div className="main-container">
@@ -68,6 +67,18 @@ export class Home extends Component {
               <button>Post</button>
             </div>
           </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/volunteers-cleaning-park-picture-id986900214?k=6&m=986900214&s=612x612&w=0&h=XaXvsvgJt1keusMMkOyXF8xpOxVLI3znd0SWRFSYxvY=" />
+          </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/donate-to-the-poor-homeless-still-seen-in-society-concept-of-charity-picture-id1082704324?k=6&m=1082704324&s=612x612&w=0&h=o4mrYDjCvudoAexUUthn8GtB2Q_hHasHmek7YHPy0cw=" />
+          </div>
+          <div className="last-post">
+            <p className="post-title">Had a great time with friends volunteering at the soup kitchen</p>
+            <img className="image-post" src="https://media.istockphoto.com/photos/people-planting-tree-in-park-picture-id1022255954?k=6&m=1022255954&s=612x612&w=0&h=kGMlSfixvVec2FSsrmoE785K5_m4pPcj0tYl3TdKYvk=" />
+          </div>
         </div>
      
         <div className="middle-container">
@@ -76,16 +87,13 @@ export class Home extends Component {
             return (
               <div key={++i} className="other-users-post-container">
                 <div className="profile-pic-and-name-container">
-                  <img src={`https://randomuser.me/api/portraits/women/${i}.jpg`}/>
+                  <img classname="profile-pic" src={`https://randomuser.me/api/portraits/women/${i}.jpg`}/>
                   <div>
-                    <p>{`${users[friendID].first_name}${users[friendID].last_name}`}</p>
+                    <p>{`${users[friendID].first_name} ${users[friendID].last_name}`}</p>
                     <p>{`${user.events[0].title}`}</p>
                     <p>{`${user.events[0].description}`}</p>
                   </div>
                     )
-               
-
-              
                 </div>
               </div>
            )
